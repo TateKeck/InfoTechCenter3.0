d4616e5d75ab7e93f4bd19e191e5b74b480e38fc
Tate keck <tatekeckcomputer@gmail.com>
2022-10-03 05:14:38 -0700

    Add Print Statement To Welcome Visitors

---
+++ OperationFury.py
@@ -0,0 +1,13 @@
+# Welcome Screen
+# developer: Tate Keck
+#Version: 1.0
+
+"""
+Our Welcome Screen Will Start Our Program Letting
+ Drivers Know That The InfoTechCenter OS is loading
+"""
+
+
+print("\n\nwelcome to Operation Furry InfoTechCenter")
+
+print("\nOperation Furry's Operation System Is Booting Up")
