6bec3fbe3887fd17e7ed584cfce84af01febac38
Tate keck <tatekeckcomputer@gmail.com>
2022-10-05 05:38:49 -0700

    whether initial commit

---
+++ OperationFury.py
@@ -0,0 +1,10 @@
+#Wether
+# developer:Tate Keck
+#version 1.0
+
+"""
+create a function for our current weather using the
+ random.choice function to determine what the weather
+is picking from a list _ using lf, elif and else statements
+to check the condition and print a specific
+"""
