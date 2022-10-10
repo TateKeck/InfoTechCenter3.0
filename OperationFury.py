3207b557475d3f5dcfb9c4e64bc2300998af0b27
Tate keck <tatekeckcomputer@gmail.com>
2022-10-10 05:39:53 -0700

    initial gas coment

---
+++ OperationFury.py
@@ -0,0 +1,12 @@
+# Gass
+# Programer: Tate keck
+# Version 1.0
+
+"""
+Define a function to check our gas gauge and determine how far
+ we have until we need gasoline based on an if, elif, else
+ condition
+"""
+
+# import library here
+import random
