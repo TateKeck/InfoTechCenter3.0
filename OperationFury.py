6c12b55949b1c57fa0897b4a5dd89e89d255046d
Tate keck <tatekeckcomputer@gmail.com>
2022-10-11 05:39:38 -0700

    add gas station

--- OperationFury.py
+++ OperationFury.py
@@ -14,18 +14,27 @@ from time import sleep
 # gas level funcion
 
 def gaslevelgauge():
-    gasslevellist=["empty","Low","quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+    gasslevellist=["empty","low","quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
     currentgaslevel = random.choice(gasslevellist)
     return currentgaslevel
 
 # variable calling the gaslevelgague funcuion to store value once
 gaslevelindicater = gaslevelgauge()
-
+def listOfGasStations():
+    gasStation = ["Shell","Circle K","Marathon","Speedway","Meijer"]
+    gasStationNearby = random.choice(gasStation)
+    return gasStationNearby
 def gaslevelAlert():
     if gaslevelindicater == "empty":
         print("WARNIG no gas")
         sleep(1)
         print("Calling Emergency Contact")
+    elif gaslevelindicater == "low":
+        print("Warning")
+        print("Your gas tank is extremely low, checking google Maps for the closest gas station. ")
+        print("The closest gas station is ",listOfGasStations())
+
+
 
 
 
