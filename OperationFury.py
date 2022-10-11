01b8d4040087795ec56241b3cd280acc3ab8779c
Tate keck <tatekeckcomputer@gmail.com>
2022-10-11 05:21:15 -0700

    making things work

--- OperationFury.py
+++ OperationFury.py
@@ -10,8 +10,23 @@ Define a function to check our gas gauge and determine how far
 
 # import library here
 import random
-
+from time import sleep
 # gas level funcion
 
 def gaslevelgauge():
-    
+    gasslevellist=["empty","Low","quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+    currentgaslevel = random.choice(gasslevellist)
+    return currentgaslevel
+
+# variable calling the gaslevelgague funcuion to store value once
+gaslevelindicater = gaslevelgauge()
+
+def gaslevelAlert():
+    if gaslevelindicater == "empty":
+        print("WARNIG no gas")
+        sleep(1)
+        print("Calling Emergency Contact")
+
+
+
+gaslevelAlert()
